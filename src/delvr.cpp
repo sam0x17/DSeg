5bd0ba1ed6a9a80335a7869130e7e10cd71d80a8
Sam Kelly <sam@durosoft.com>
2016-05-06 20:03:34 -0400

    Hello world

---
+++ src/delvr.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "assert.h"
+
+int main(int argc, char** argv) {
+  std::cout << "Delvr 1.0 Loaded." << std::endl;
+  std::cout << "Copyright Sam Kelly" << std::endl;
+  std::cout << std::endl;
+}
