86ed8a3a5545899af5164997c9147308ed700713
Sam Kelly <sam@durosoft.com>
2016-05-07 00:17:14 -0400

    Add compilation to submodule script

--- scripts/install_submodules.sh
+++ scripts/install_submodules.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "Initializing and fetching gSLICr fork submodule..."
+echo "fetching gSLICr fork submodule..."
 cd ../
 git submodule init
 git submodule update
+echo "compiling gSLICr fork..."
+cd gSLICr || exit 1
+mkdir -p build || exit 1
+rm build -rf
+mkdir -p build || exit 1
+cd build || exit 1
+echo "running CMake..." || exit 1
+cmake ../ || exit 1
+echo "running make..." || exit 1
+make -j4 || exit 1
 echo "done"
