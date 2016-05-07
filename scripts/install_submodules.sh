fca971e8734e98662c3e6294fe7f95b776489d73
Sam Kelly <sam@durosoft.com>
2016-05-07 11:55:48 -0400

    Last commits from gSLICr before removal

--- scripts/install_submodules.sh
+++ scripts/install_submodules.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo "fetching gSLICr fork submodule..."
 cd ../
-git submodule init
-git submodule update
+#git submodule init
+#git submodule update
 echo "compiling gSLICr fork..."
 cd gSLICr || exit 1
 mkdir -p build || exit 1
-rm build -rf
-mkdir -p build || exit 1
+rm -rf build/*
 cd build || exit 1
 echo "running CMake..." || exit 1
 cmake ../ || exit 1
