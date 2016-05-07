bbc62ef84826de1e7d9ca23c93574cad8d4ceb4d
Sam Kelly <sam@durosoft.com>
2016-05-07 06:10:36 -0400

    Update install packages script

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "installing required packages..."
 sudo apt-get update
-sudo apt-get install build-essential pigz cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev p7zip-full
+sudo apt-get install libgtk2.0-dev pkg-config build-essential pigz cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev p7zip-full
