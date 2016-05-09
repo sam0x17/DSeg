5f8c4b65318eacca1f21d36e63244f5578595cba
Sam Kelly <sam@durosoft.com>
2016-05-09 11:43:56 -0400

    Add data generating lines to build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -3,4 +3,10 @@
 cd bin
 #rm contours*
 #rm patch*
-./delvr genfeats translucent data/imgs/hamina\ 128x128\ m-30\ crop-t hamina.dfeats
+#./delvr genfeats translucent data/imgs/hamina\ 128x128\ m-30\ crop-t hamina.dfeats
+#./delvr genfeats translucent data/imgs/m4a1_s\ 128x128\ m-30\ crop-t m4a1.dfeats
+#./delvr genfeats translucent data/imgs/halifax\ 128x128\ m-30\ crop-t halifax.dfeats
+#./delvr genfeats translucent data/imgs/kirov\ 128x128\ m-30\ crop-t kirov.dfeats
+#./delvr genfeats translucent data/imgs/kuznet\ 128x128\ m-30\ crop-t kuznet.dfeats
+#./delvr genfeats translucent data/imgs/udaloy\ 128x128\ m-30\ crop-t udaloy.dfeats
+#./delvr genfeats translucent data/imgs/sovddg\ 128x128\ m-30\ crop-t sovddg.dfeats
